18694930672184938c8cee0af0aab8aa1ef77e49
Brevven <brevingame@gmail.com>
2022-01-30 21:10:20 -0800

    zircon in rocks

--- data-updates.lua
+++ data-updates.lua
@@ -7,6 +7,16 @@ require("strange-matter")
 
 local util = require("data-util");
 
-util.add_minable_result("simple-entity", "rock-huge", {name="zircon", amount_min = 15, amount_max=25})
-util.add_minable_result("simple-entity", "sand-rock-big", {name="zircon", amount_min = 15, amount_max=25})
-util.add_minable_result("simple-entity", "rock-big", {name="zircon", amount_min = 15, amount_max=25})
+
+-- Add zircon to rocks
+local min = 15
+local max = 23
+if mods["space-exploration"] then
+  min = 6
+  max = 10
+end
+for _, e in pairs(data.raw["simple-entity"]) do
+  if (e.subgroup == "grass" or e.subgroup == "wrecks") and e.name:match("rock%-") then
+    util.add_minable_result("simple-entity", e.name, {name="zircon", amount_min = min, amount_max=max})
+  end
+end
