53d5b197859d84562e2bf682159de8c09f2eb981
Brevven <brevingame@gmail.com>
2022-02-01 21:19:57 -0800

    rocks tweak, alien biomes

--- data-updates.lua
+++ data-updates.lua
@@ -9,14 +9,16 @@ local util = require("data-util");
 
 
 -- Add zircon to rocks
-local min = 15
-local max = 23
-if mods["space-exploration"] then
-  min = 6
-  max = 10
+local min = 9
+local max = 11
+if mods["alien-biomes"] then
+  min = 5
+  max = 7
 end
 for _, e in pairs(data.raw["simple-entity"]) do
   if (e.subgroup == "grass" or e.subgroup == "wrecks") and e.name:match("rock%-") then
-    util.add_minable_result("simple-entity", e.name, {name="zircon", amount_min = min, amount_max=max})
+    local adjusted_max = e.name:match("huge") and max * 2 or max
+    local adjusted_min = e.name:match("huge") and min * 2 or min
+    util.add_minable_result("simple-entity", e.name, {name="zircon", amount_min=adjusted_min, amount_max=adjusted_max})
   end
 end
