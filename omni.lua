c6fb7c2f768e1df774dc93a062dc45588c1ba994
oorzkws <65210810+oorzkws@users.noreply.github.com>
2022-04-09 11:46:20 +0000

    Gracefully fallback if iron-ore isn't found
    Fixes startup issue with some mod combos plus omnimatter

--- omni.lua
+++ omni.lua
@@ -1,3 +1,3 @@
 if omni and omni.matter then
-  omni.matter.add_resource("zircon", omni.matter.get_ore_tier("iron-ore"))
+  omni.matter.add_resource("zircon", omni.matter.get_ore_tier("iron-ore") or 1)
 end
