d3072c0393ec036421a80a3b3dfbf75865696f57
Brevven <brevingame@gmail.com>
2022-06-04 23:10:29 -0700

    ploppable rocks require zircon

--- data-updates.lua
+++ data-updates.lua
@@ -4,21 +4,7 @@ require("zircon-matter")
 require("map-gen-preset-updates")
 require("omni")
 require("strange-matter")
+require("zircon-rocks")
 
-local util = require("data-util");
 
 
--- Add zircon to rocks
-local min = 9
-local max = 11
-if mods["alien-biomes"] then
-  min = 5
-  max = 7
-end
-for _, e in pairs(data.raw["simple-entity"]) do
-  if (e.subgroup == "grass" or e.subgroup == "wrecks") and e.name:match("rock%-") then
-    local adjusted_max = e.name:match("huge") and max * 2 or max
-    local adjusted_min = e.name:match("huge") and min * 2 or min
-    util.add_minable_result("simple-entity", e.name, {name="zircon", amount_min=adjusted_min, amount_max=adjusted_max})
-  end
-end
